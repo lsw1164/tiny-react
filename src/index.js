4f528928ecff8d83c0bcf7bdff42c870665feb40
lsw1164 <qkqkfo@gmail.com>
2020-12-21 04:27:28 +0900

    Implement tiny-react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Tiny React");
+/* @jsx createElement */
+// default : React.createElement
+
+import { createElement, render } from "./react.js";
+
+function Title(props) {
+  return (
+    <div>
+      <h2>This is h2 tag</h2>
+      <p>This is p tag</p>
+    </div>
+  );
+}
+
+render(<Title />, document.querySelector("#root"));
---
+++ src/react.js
@@ -0,0 +1,39 @@
+function renderRealDOM(vdom) {
+  if (vdom === undefined) return;
+  if (typeof vdom === "string") {
+    return document.createTextNode(vdom);
+  }
+
+  const $el = document.createElement(vdom.tagName);
+
+  vdom.children.map(renderRealDOM).forEach((node) => {
+    $el.appendChild(node);
+  });
+
+  return $el;
+}
+
+export function render(vdom, container) {
+  container.appendChild(renderRealDOM(vdom));
+}
+
+/*
+export const render = function () {
+  let prevVdom = null;
+
+  return function (nextVdom, container) {
+    if (prevVdom === null) {
+      prevVdom = nextVdom;
+    }
+    //diff
+    container.appendChild(renderRealDOM(vdom));
+  };
+};
+*/
+
+export function createElement(tagName, props, ...children) {
+  if (typeof tagName === "function") {
+    return tagName.apply(null, [props, ...children]);
+  }
+  return { tagName, props, children };
+}
