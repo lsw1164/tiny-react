251f20e54da53f7ae0fa5c945fdcac0976f67134
lsw1164 <qkqkfo@gmail.com>
2021-08-06 03:36:28 +0900

    :bug: Append dom

--- src/TinyReact.js
+++ src/TinyReact.js
@@ -27,83 +27,20 @@ function createTextElement(text) {
   };
 }
 
-function isProperty(key) {
-  return key !== "children";
-}
-
-function createDom(fiber) {
-  const dom =
-    fiber.type === TEXT_ELEMENT
-      ? document.createTextNode("")
-      : document.createElement(fiber.type);
-  Object.keys(fiber.props)
-    .filter(isProperty)
-    .forEach((propName) => (dom[propName] = fiber.props[propName]));
-  return dom;
-}
-
-let nextUtilOfWork = null;
-
 function render(element, container) {
-  nextUtilOfWork = {
-    dom: container,
-    props: {
-      children: [element],
-    },
-  };
-}
+  const dom =
+    element.type === TEXT_ELEMENT
+      ? document.createTextNode("")
+      : document.createElement(element.type);
 
-function workLoop(deadline) {
-  let shouldYield = false;
-  while (nextUtilOfWork && !shouldYield) {
-    nextUtilOfWork = performUtilOfWork(nextUtilOfWork);
-    shouldYield = deadline.timeRemaining() < 1;
-  }
+  const isProperty = (key) => key !== "children";
 
-  requestIdleCallback(workLoop);
-}
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach((propName) => (dom[propName] = element.props[propName]));
 
-requestIdleCallback(workLoop);
-
-function performUtilOfWork(fiber) {
-  if (!fiber.dom) {
-    fiber.dom = createDom(fiber);
-  }
-
-  if (fiber.parent) {
-    fiber.parent.dom.appendChild(fiber.dom);
-  }
-  const elements = fiber.props.children;
-  let index = 0;
-  let prevSibling = null;
-  while (index < elements.length) {
-    const element = elements[index];
-    const newFiber = {
-      type: element.type,
-      props: element.props,
-      parent: fiber,
-      dom: null,
-    };
-  }
-
-  if (index === 0) {
-    fiber.child = newFiber;
-  } else {
-    prevSibling.sibling = newFiber;
-  }
-  prevSibling = newFiber;
-  index++;
-
-  if (fiber.child) {
-    return fiber.child;
-  }
-  let nextFiber = fiber;
-  while (nextFiber) {
-    if (nextFiber.sibling) {
-      return nextFiber.sibling;
-    }
-    nextFiber = nextFiber.parent;
-  }
+  element.props.children.forEach((child) => render(child, dom));
+  container.appendChild(dom);
 }
 
 export default TinyReact;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@
 import TinyReact from "./TinyReact.js";
 
 const element = (
-  <div style="background: blue">
+  <div style="background: skyblue">
     <h1>Hello world</h1>
     <h2 style="color: red">Tiny React</h2>
+    <div>Div1</div>
+    <div>Div2</div>
   </div>
 );
 
